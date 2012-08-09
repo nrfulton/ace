9586d9c5947ff307aa9e2da11930bab8a64c236c
nrfulton <nathan.fulton@gmail.com>
2012-08-09 15:58:59 -0500

    better inline documentation of the code generator, with an eye towards anyone who needs to change things due to new additions to the OpenCL specification.

--- pycparserext/typechecker/ocl_builtins_generator.py
+++ pycparserext/typechecker/ocl_builtins_generator.py
@@ -1,5 +1,3 @@
-#Generates code for OCL built-in types.
-
 from pycparserext.typechecker import Type
 from pycparserext.typechecker import GENTYPES
 from pycparserext.typechecker import GENTYPES
@@ -14,9 +12,11 @@ class ParserException(Exception):
         self.message = msg
 
 class Param(object):
-    """A single parameter in a signature."""
+    """An argument in a function's signature.
+    
+    Param parses the information necessary to instantiate a typechecker.Type 
+    out of a line in ocl_functions.txt."""
     def __init__(self, p):
-        """Parses a type_checker.Type out of a line in ocl_functions.txt"""
         ret = Type("")
         #Handle pointer types
         if p.endswith('*'):
@@ -33,6 +33,20 @@ class Param(object):
         self.type = ret
     
     def generate_valid_types(self):
+        """Generates a list that associates gentypes with actual OpenCL types.
+        
+        The OpenCL spec, and particularly \S 6.11.x, is written using gentypes
+        as shorthands for sets of types. oct_functions.txt adopts this as well.
+        This function associates each of those types with all of the valid
+        OpenCL types. Example:
+            sgentype -> int,int2,...,char,char2,...,long,long2,... and so on.
+        
+        The information necessary to update this function can be found be reading
+        through the preface to each of the 6.11.x sections in the OpenCL 
+        specification.
+        
+        This class generates instances of pycparserext.typechecker.BuiltinFnArg
+        """
         ret     = ""
         types   = list()
         stypes  = SGENTYPES
@@ -65,10 +79,11 @@ class Param(object):
         return ret
 
     def var_name(self):
+        """The name for this list in generated code."""
         return "builtinfnargtype_%s" % self.num 
         
     def generate_code(self, num):
-        """Generates code for this type; the variable name depends on `1num`1"""
+        """Generates code for this type; the variable name depends on num."""
         self.num = num
         subs = self.generate_valid_types()
         print "%s = BuiltinFnArgType(\"%s\",(%s))" % (self.var_name(),
@@ -80,6 +95,7 @@ class Param(object):
 Param = cypy.intern(Param)
 
 class ParamList(object):
+    """A signature that may be shared by multiple functions."""
     def __init__(self, params, return_type):
         self.params = params
         self.return_type = return_type
@@ -106,7 +122,15 @@ class ParamList(object):
                                               return_type)
 
 class OclFunctionLine(object):
-    """A line in ocl_functions.txt"""
+    """Represents a line in ocl_functions.txt. Used for parsing.
+    
+    The format of each line in ocl_functions.txt is:
+    function_name.argtype1,argtype2,...,argtypen.return_type.
+    Each of the argtypes and the return type may be either an OpenCL type,
+    or an abbreviation for a set of types (see Param.generate_valid_types)
+    
+    This class generates instances of pycparserext.typechecker.BuiltinFnArgLists
+    """
     def __init__(self, line):
         line = line.split(".")
         if len(line) != 3:
@@ -122,7 +146,12 @@ class OclFunctionLine(object):
         
     
     def generate_param_list(self, i, param_lists):
-        """Generates a unique param list for each argument."""
+        """Generates all necessary param lists.
+        
+        We significantly reduce the amount of generated code by sharing param
+        lists between functions that have identical signatures. For instance,
+        min and max will share a param list.
+        """
         found_list = False
         for list in param_lists:
             match = True
@@ -143,6 +172,7 @@ class OclFunctionLine(object):
             new_list.generate_code(i)
 
     def generate_code(self, generated_functions):
+        """Generates params, param lists and functions."""
         if not (self.name in generated_functions):
             generated_functions.append(self.name)
             print "builtin_fns[\"%s\"] = BuiltinFn(\"%s\",%s)" % \
@@ -155,6 +185,35 @@ class OclFunctionLine(object):
                      self.param_list.var_name())
 
 class CodeGenerator(object):
+    """Generates code for pychparserext.typechecker.
+    
+    This class generates a large hashmap that will be used to create builtin
+    functions. It uses ocl_functions.txt, which is a transcription of the OpenCL
+    specification (\S 6.11.x -- see ocl_functions.txt).
+    
+    Each line is parsed, and a minimal set of Param and ParamList objects are
+    created. Each Param represents an argument or set of valid arguments, and
+    each ParamList is a valid signature.
+    
+    Code is then generated in the following order:
+    
+    Params: Each unique paramter (gentype, int, float2, etc.) that is
+    reified in ocl_functions.txt has only one Param instance.
+    
+    ParamLists: Similarly, each unique signature has only one parameter
+    list objct. A parameter list is very close to a signure, but is distinct 
+    because a ParamList can also be a set of signatures. For instance,
+        min.gentype.gentype
+    is actually a large set of signatures. However, there is only one 
+    parameterlist for this function. See the docstring for ParamList for more
+    details.
+    
+    Functions: Each function is a name together with a collection of valid
+    signatures. The generated code represents this as a hashmap of lists,
+    where the key is the function name and the value is a list of 
+    paramter lists. In pycparserext.typechecker, each entry in this 
+    hash map is turned into an instance of BuiltinFn.
+    """
     def __init__(self):
         self.types = list() #BuiltInFnArgTypes generated by Param
         self.lines = list() #OclFunctionLines
@@ -168,6 +227,8 @@ class CodeGenerator(object):
             self.types.append(line.return_type)
     
     def generate_code(self):
+
+         
         print "### BEGIN GENERATED CODE ###"
         print "#Parameters"
         for i,t in enumerate(self.types):
@@ -187,6 +248,7 @@ class CodeGenerator(object):
         print "### END GENERATED CODE ###"
         
 
+#Generate code using ocl_functions.txt.
 try:
     cg = CodeGenerator()
     file = open("ocl_functions.txt", "r")
